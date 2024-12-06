582e1e5ee4d975b7d0aeab41df365eece0e5abe8
Boris <shevnin.boris2@yandex.ru>
2024-12-06 12:30:59 +0300

    feat: add About section (need design fixes)

---
+++ app/components/sections/aboutUs/AboutUs.vue
@@ -0,0 +1,53 @@
+<template>
+  <section :class="styles['about-section']">
+    <div :class="styles['about-section__content']">
+      <!-- Левый блок: изображение -->
+      <NuxtImg
+        src="/aboutUsImage.svg"
+        alt="about us image"
+        class="about-section__image"
+        width="677"
+        height="558"
+      />
+
+      <!-- Правый блок: текст и информация -->
+      <div :class="styles['about-section__details']">
+        <h2 :class="styles['about-section__title']">О нас</h2>
+        <p :class="styles['about-section__description']">
+          Компания, которой можно доверить всё — от частного ремонта до
+          строительства масштабных муниципальных объектов. Наши специалисты
+          выполняли работы для промышленных предприятий и инфраструктурных
+          проектов. Благодаря собственному ПТО и слаженной команде мы
+          обеспечиваем надёжность, точность и выполнение сроков как для крупных
+          организаций, так и для частных заказчиков.
+        </p>
+
+        <!-- Блок со статистикой -->
+        <div :class="styles['about-section__stats']">
+          <div :class="styles['about-section__stat']">
+            <h3 :class="styles['about-section__stat-title']">
+              11 лет на рынке
+            </h3>
+            <p :class="styles['about-section__stat-description']">
+              Наша компания работает 11 лет и зарекомендовала себя как надёжный
+              партнёр в сфере строительства.
+            </p>
+          </div>
+          <div :class="styles['about-section__stat']">
+            <h3 :class="styles['about-section__stat-title']">100+ проектов</h3>
+            <p :class="styles['about-section__stat-description']">
+              Реализовали более 100 успешных проектов, что подтверждает наш
+              профессионализм и опыт работы.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import styles from "./AboutUs.module.scss";
+</script>
+
+<style lang="scss" module></style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,14 @@
   <main>
     <LandingHeader />
     <MainSection />
+    <AboutUs />
   </main>
 </template>
 
 <script setup>
 import MainSection from "~/app/components/sections/main/Main.vue";
 import LandingHeader from "~/app/components/header/LandingHeader.vue";
+import AboutUs from "~/app/components/sections/aboutUs/AboutUs.vue";
 </script>
 
 <style lang="scss" scoped></style>
